663d83524869a44b89590533adbada021c8ab20b
yhson135 <42964811+yhson135@users.noreply.github.com>
2021-12-07 09:56:36 +0900

    Add files via upload

---
+++ train.py
@@ -0,0 +1,224 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import time
+import os
+from tqdm import tqdm
+from model import ViT_UNet
+from UnetPlusPlus_model import Nested_UNet
+from dataset import (train_loader, val_loader)
+from utils import *
+from datetime import datetime
+import segmentation_models_pytorch as smp
+
+
+
+
+
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+
+
+def fit(epochs, model, train_loader, val_loader, criterion, optimizer, scheduler, device, patch=False):
+    torch.cuda.empty_cache()
+    train_losses = []
+    test_losses = []
+    val_iou = [];
+    val_acc = []
+    train_iou = [];
+    train_acc = []
+    lrs = []
+    min_loss = np.inf
+    decrease = 1;
+    not_improve = 0
+
+    model.to(device)
+    fit_time = time.time()
+    for e in range(epochs):
+        since = time.time()
+        running_loss = 0
+        iou_score = 0
+        accuracy = 0
+        # training loop
+        model.train()
+        for i, data in enumerate(tqdm(train_loader)):
+            # training phase
+            image_tiles, mask_tiles = data
+            if patch:  # patch for SwinTransformer
+                bs, n_tiles, c, h, w = image_tiles.size()
+
+                image_tiles = image_tiles.view(-1, c, h, w)
+                mask_tiles = mask_tiles.view(-1, h, w)
+
+            image = image_tiles.to(device);
+            mask = mask_tiles.to(device);
+            # forward
+            output = model(image)
+            loss = criterion(output, mask)
+            # evaluation metrics
+            iou_score += mIoU(output, mask)
+            accuracy += pixel_accuracy(output, mask)
+            # backward
+            loss.backward()
+            optimizer.step()  # update weight
+            optimizer.zero_grad()  # reset gradient
+
+            # step the learning rate
+            lrs.append(get_lr(optimizer))
+            scheduler.step()
+
+            running_loss += loss.item()
+
+        else:
+            model.eval()
+            test_loss = 0
+            test_accuracy = 0
+            val_iou_score = 0
+            # validation loop
+            with torch.no_grad():
+                for i, data in enumerate(tqdm(val_loader)):
+                    # reshape to 9 patches from single image, delete batch size
+                    image_tiles, mask_tiles = data
+
+                    if patch:
+                        bs, n_tiles, c, h, w = image_tiles.size()
+
+                        image_tiles = image_tiles.view(-1, c, h, w)
+                        mask_tiles = mask_tiles.view(-1, h, w)
+
+                    image = image_tiles.to(device);
+                    mask = mask_tiles.to(device);
+                    output = model(image)
+                    # evaluation metrics
+                    val_iou_score += mIoU(output, mask)
+                    test_accuracy += pixel_accuracy(output, mask)
+                    # loss
+                    loss = criterion(output, mask)
+                    test_loss += loss.item()
+
+            # calculate mean for each batch
+            train_losses.append(running_loss / len(train_loader))
+            test_losses.append(test_loss / len(val_loader))
+
+            if min_loss > (test_loss / len(val_loader)):
+                print('Loss Decreasing.. {:.3f} >> {:.3f} '.format(min_loss, (test_loss / len(val_loader))))
+                min_loss = (test_loss / len(val_loader))
+                decrease += 1
+                if decrease % 5 == 0:
+                    print('saving model...')
+                    save_dir = "/home/user/yh/GraduateClass/NeuralProject/Ass2/save_model/1206"
+                    os.makedirs(save_dir, exist_ok=True)
+                    torch.save(model.state_dict(), save_dir + '/yh_DeepLabV3_timm-regnety_320_mIoU-{:.3f}.pt'.format(val_iou_score / len(val_loader)))
+
+            if (test_loss / len(val_loader)) > min_loss:
+                not_improve += 1
+                min_loss = (test_loss / len(val_loader))
+                print(f'Loss Not Decrease for {not_improve} time')
+                if not_improve == 70:
+                    print('Loss not decrease for 70 times, Stop Training')
+                    break
+
+            # iou
+            val_iou.append(val_iou_score / len(val_loader))
+            train_iou.append(iou_score / len(train_loader))
+            train_acc.append(accuracy / len(train_loader))
+            val_acc.append(test_accuracy / len(val_loader))
+            print("Epoch:{}/{}..".format(e + 1, epochs),
+                  "Train Loss: {:.3f}..".format(running_loss / len(train_loader)),
+                  "Val Loss: {:.3f}..".format(test_loss / len(val_loader)),
+                  "Train mIoU:{:.3f}..".format(iou_score / len(train_loader)),
+                  "Val mIoU: {:.3f}..".format(val_iou_score / len(val_loader)),
+                  "Train Acc:{:.3f}..".format(accuracy / len(train_loader)),
+                  "Val Acc:{:.3f}..".format(test_accuracy / len(val_loader)),
+                  "Time: {:.2f}m".format((time.time() - since) / 60))
+
+    history = {'train_loss': train_losses, 'val_loss': test_losses,
+               'train_miou': train_iou, 'val_miou': val_iou,
+               'train_acc': train_acc, 'val_acc': val_acc,
+               'lrs': lrs}
+    print('Total time: {:.2f} m'.format((time.time() - fit_time) / 60))
+    return history
+
+
+max_lr = 1e-3
+epoch = 400
+weight_decay = 1e-4
+
+# Set GPU number
+os.environ["CUDA_VISIBLE_DEVICES"] = "5"
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")  # PyTorch v0.4.0
+
+# model = ViT_UNet(img_size=(512, 768))
+
+
+# model = smp.UnetPlusPlus(
+#     encoder_name="resnet34",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+#     encoder_weights="imagenet",     # use `imagenet` pre-trained weights for encoder initialization
+#     in_channels=3,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
+#     classes=23,                      # model output channels (number of classes in your dataset)
+# )
+
+
+model = smp.DeepLabV3Plus(
+    encoder_name="timm-regnety_320",  # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+    encoder_weights="imagenet",     # use `imagenet` pre-trained weights for encoder initialization
+    in_channels=3,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
+    classes=23,                      # model output channels (number of classes in your dataset)
+)
+
+
+
+
+timestamp = datetime.today().strftime("_%Y%m%d%H%M%S")
+# remark = f"Baseline_{timestamp}"
+remark = f"DeepLab_mobilenet_v2_{timestamp}"
+print("remark : ", remark)
+print("timestamp : ", timestamp)
+print("device : ", device)
+
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.AdamW(model.parameters(), lr=max_lr, weight_decay=weight_decay)
+sched = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr, epochs=epoch,
+                                            steps_per_epoch=len(train_loader))
+
+history = fit(epoch, model, train_loader, val_loader, criterion, optimizer, sched, device=device)
+
+torch.save(model.state_dict(), f"save_model/1206/ViT_UNet_{remark}.pt")
+
+def plot_loss(history):
+    plt.plot(history['val_loss'], label='val', marker='o')
+    plt.plot(history['train_loss'], label='train', marker='o')
+    plt.title('Loss per epoch');
+    plt.ylabel('loss');
+    plt.xlabel('epoch')
+    plt.legend(), plt.grid()
+    plt.show()
+
+
+def plot_score(history):
+    plt.plot(history['train_miou'], label='train_mIoU', marker='*')
+    plt.plot(history['val_miou'], label='val_mIoU', marker='*')
+    plt.title('Score per epoch');
+    plt.ylabel('mean IoU')
+    plt.xlabel('epoch')
+    plt.legend(), plt.grid()
+    plt.show()
+
+
+def plot_acc(history):
+    plt.plot(history['train_acc'], label='train_accuracy', marker='*')
+    plt.plot(history['val_acc'], label='val_accuracy', marker='*')
+    plt.title('Accuracy per epoch');
+    plt.ylabel('Accuracy')
+    plt.xlabel('epoch')
+    plt.legend(), plt.grid()
+    plt.show()
+
+plot_loss(history)
+plot_score(history)
+plot_acc(history)
